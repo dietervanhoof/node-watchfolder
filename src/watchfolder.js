eacade94992bb7836cc43968aa4de3722348ddd3
dietervanhoof <dieter.vanhoof@c4j.be>
2017-02-09 11:36:43 +0100

    Added waiting for file to complete copying before accepting it

--- src/watchfolder.js
+++ src/watchfolder.js
@@ -5,10 +5,13 @@ const FileRecognizer = require("./util/file_recognizer");
 const options = require("./util/cmdargs").parseArguments();
 const Publisher = require("./amqp/publisher");
 const Generator = require("./util/message_generator");
+const fs = require("fs");
 
 const generator = new Generator(options);
 const publisher = new Publisher(options);
 let fileindex = new FileIndex(options, new FileRecognizer(options), publisher, generator);
+
+const fileCopyDelaySeconds = 3;
 
 publisher.initialize().then(startWatching);
 
@@ -18,6 +21,34 @@ function startWatching() {
         // Ignore sub-folders
         return RegExp(options.folder + '.+/').test(path)
     }}).on('add', (event, path) => {
-        fileindex.add_file(event, fileindex.determine_file_type(event, options), options, publisher, generator);
+        tryWatchFile(event);
     });
 };
+
+function tryWatchFile(event) {
+    return new Promise((fulfill, reject) => {
+        fs.stat(event, function (err, stat) {
+            if (err){
+                log.error('Error watching file for copy completion. ERR: ' + err.message);
+                log.error('Error file not processed. PATH: ' + event);
+                reject(err);
+            } else {
+                fulfill(setTimeout(checkFileCopyComplete, fileCopyDelaySeconds*1000, event, stat));
+            }
+        });
+    });
+}
+
+function checkFileCopyComplete(path, prev) {
+    fs.stat(path, function (err, stat) {
+        if (err) {
+            throw err;
+        }
+        if (stat.mtime.getTime() === prev.mtime.getTime()) {
+            fileindex.add_file(path, fileindex.determine_file_type(path, options), options, publisher, generator)
+        }
+        else {
+            setTimeout(checkFileCopyComplete, fileCopyDelaySeconds*1000, path, stat);
+        }
+    });
+}
