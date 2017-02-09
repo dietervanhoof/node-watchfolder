c50020f3550e10f97330832f92840137c8209c5c
dietervanhoof <dieter.vanhoof@c4j.be>
2017-02-09 19:41:53 +0100

    Removed unnecessary function wrap

--- src/util/file_utils.js
+++ src/util/file_utils.js
@@ -1,46 +1,30 @@
 const mv = require("mv");
 const path = require("path");
 
-(() => {
-    function calcMD5 (file) {
-        var fd = fs.openSync (file, 'r');
-        var hash = crypto.createHash ('md5');
-        var buffer = new Buffer (1024);
-        var n = 0;
+function getFileName(file_path) {
+    return path.basename(file_path);
+}
 
-        while ( nr = fs.readSync (fd, buffer, 0, buffer.length) ) {
-            hash.update (buffer.slice (0, nr));
-        }
-        fs.close(fd);
-        return hash.digest ('hex');
-    };
+function getFolder(file_path) {
+    return path.dirname(file_path);
+}
 
-    function getFileName(file_path) {
-        return path.basename(file_path);
-    }
+function createFullPath(folder, file) {
+    return folder.endsWith('/') ? folder + file : folder + '/' + file;
+}
 
-    function getFolder(file_path) {
-        return path.dirname(file_path);
-    }
+function appendFolder(file_path, folder) {
+    return file_path.endsWith('/') ? file_path + folder : file_path + '/' + folder;
+}
 
-    function createFullPath(folder, file) {
-        return folder.endsWith('/') ? folder + file : folder + '/' + file;
-    }
+function moveFile(sourcePath, destinationPath, cb) {
+    mv(sourcePath, destinationPath, {mkdirp: true}, cb);
+}
 
-    function appendFolder(file_path, folder) {
-        return file_path.endsWith('/') ? file_path + folder : file_path + '/' + folder;
-    }
-
-    function moveFile(sourcePath, destinationPath, cb) {
-        mv(sourcePath, destinationPath, {mkdirp: true}, cb);
-    }
-
-    module.exports = {
-        calcMD5: calcMD5,
-        moveFile: moveFile,
-        appendFolder: appendFolder,
-        createFullPath: createFullPath,
-        getFileName: getFileName,
-        getFolder: getFolder
-    };
-}) ();
+module.exports = {
+    moveFile: moveFile,
+    appendFolder: appendFolder,
+    createFullPath: createFullPath,
+    getFileName: getFileName,
+    getFolder: getFolder
+};
